a17835aed042761423726933c2354a6ba106926a
cverma-gainsight <cverma@gainsight.com>
2023-05-02 17:55:04 +0530

    commit4

--- js/script.js
+++ js/script.js
@@ -15,23 +15,19 @@ function myFunction() {
 aptrinsic("identify",
   {
   //User Fields
-    "id": "unique-user-id", // Required for logged in app users
-    "email": "userEmail@address.com",
-    "firstName": "John",
-    "lastName": "Smith",
-    "signUpDate": 1522697426479, //unix time in ms
-    "plan" : "gold", //Custom attributes - please create those custom attributes in Aptrinsic via Account Settings to be tracked.
-    "price" : 95.5,
-    "userHash": "" // optional transient for HMAC identification
+    "id": x, // Required for logged in app users
+    "email": y,
+    "firstName": c,
+    "lastName": "",
   },
   {
   //Account Fields
-    "id":"IBM", //Required
-    "name":"International Business Machine",
+    "id":"gs", //Required
+    "name":"Gainsight",
     "Program": "Platinum" // flat custom attributes
  });
      alert ("Valid User");
-     location.href = "contact.htm";
+     location.href = "contact.html";
   } else if (y == '') {
       alert ("Empty response");
   }
