d467ad8c1f8991e3fef100717e6daf6928f016a8
cverma-gainsight <cverma@gainsight.com>
2023-05-02 17:24:09 +0530

    commit2

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+  (function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
+      (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
+    var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
+    var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
+  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-KLSX5CWHR5PW-2");
+
+function myFunction() {
+  // Get the value of the input field with id="numb"
+   var x = document.getElementById("name").value;
+  var y = document.getElementById("email").value;   
+
+ if (y == 'cverma@gainsight.com') {
+ alert ("Valid User");
+  } else if (y == '') {
+      alert ("Empty response");
+  }
+    else{
+        
+        alert ("Invalid User");
+  }
+    }
