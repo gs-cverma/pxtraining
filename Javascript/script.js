1ddf25a726ffebc27bae5900f8cfb5f06c508394
gs-cverma <99386626+gs-cverma@users.noreply.github.com>
2025-06-05 11:18:45 +0530

    Update script.js

--- Javascript/script.js
+++ Javascript/script.js
@@ -2,7 +2,7 @@
   (function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
       (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
     var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
-    r.integrity="sha256-21a3c69a309edd917b260be6f0b3cefd8a9c6c6a2170823472a8b8a6cd1d8293";
+    r.integrity="sha256-21a3c69a309edd917b260be6f0b3cefd8a9c6c6a2170823472a8b8a6cd1d829";
     var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
   })(window,document,"https://cdnjs-web-sdk.aptrinsic.com/web-sdk/0.54.18/api/aptrinsic.js","AP-4IRRDOAOY9E3-2");
 
